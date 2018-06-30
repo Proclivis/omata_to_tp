05b7651e85da730a82855722eab2011f81dcac84
Mike Jones <proclivis@gmail.com>
2018-06-30 12:32:21 -0600

    Fix Copyright

--- DecodeDemo.cs
+++ DecodeDemo.cs
@@ -1,14 +1,23 @@
 #region Copyright
 ////////////////////////////////////////////////////////////////////////////////
-// The following FIT Protocol software provided may be used with FIT protocol
-// devices only and remains the copyrighted property of Dynastream Innovations Inc.
-// The software is being provided on an "as-is" basis and as an accommodation,
-// and therefore all warranties, representations, or guarantees of any kind
-// (whether express, implied or statutory) including, without limitation,
-// warranties of merchantability, non-infringement, or fitness for a particular
-// purpose, are specifically disclaimed.
+// Copyright 2018 Michael A Jones
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is furnished 
+// to do so, subject to the following conditions:
 //
-// Copyright 2012 Dynastream Innovations Inc.
+//The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////
 #endregion
 
